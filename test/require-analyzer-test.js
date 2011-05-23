f66788c0093693706cf788063c679ae7995e046d
Dominic Tarr <dominic.tarr@gmail.com>
2011-05-23 15:38:13 -0700

    [minor]: repair formatting to test/require-analyzer-test.js

--- test/require-analyzer-test.js
+++ test/require-analyzer-test.js
@@ -8,81 +8,81 @@
 require.paths.unshift(require('path').join(__dirname, '..', 'lib'));
 
 var fs = require('fs'),
-  path = require('path'),
-  vows = require('vows'),
-  assert = require('assert'),
-  analyzer = require('require-analyzer');
+    path = require('path'),
+    vows = require('vows'),
+    assert = require('assert'),
+    analyzer = require('require-analyzer');
 
 var rawPackages = [
-'npm',
-'semver',
-'nopt',
-'abbrev',
-'findit',
-'seq',
-'hashish',
-'traverse',
-'chainsaw'
+  'npm',
+  'semver',
+  'nopt',
+  'abbrev',
+  'findit',
+  'seq',
+  'hashish',
+  'traverse',
+  'chainsaw'
 ];
 
 var packages = [
-'findit',
-'npm',
-'semver'
+  'findit',
+  'npm',
+  'semver'
 ];
 
 vows.describe('require-analyzer').addBatch({
-"When using require-analyzer": {
-  "the analyze() method": {
-    "when passed a directory": {
-      "with a valid package.json": {
-        topic: function () {
-          analyzer.analyze({target: path.join(__dirname, '..') }, this.callback)
-        },
-        "should respond with the correct dependencies": function (err, pkgs) {
-          assert.isNull(err);
-          assert.deepEqual(Object.keys(pkgs), packages);
+  "When using require-analyzer": {
+    "the analyze() method": {
+      "when passed a directory": {
+        "with a valid package.json": {
+          topic: function () {
+            analyzer.analyze({target: path.join(__dirname, '..') }, this.callback)
+          },
+          "should respond with the correct dependencies": function (err, pkgs) {
+            assert.isNull(err);
+            assert.deepEqual(Object.keys(pkgs), packages);
+          }
         }
       }
-    }
-  },
-  "the dir() method": {
-    topic: function () {
-      var that = this;
-      analyzer.dir({ target: path.join(__dirname, '..', 'lib') }, this.callback);
     },
-    "should respond with the correct dependencies": function (err, pkgs) {
-      assert.isNull(err);
-      assert.deepEqual(pkgs, rawPackages);
-    }
-  },
-  "the package() method": {
-    topic: function () {
-      analyzer.package({ target: path.join(__dirname, '..') }, this.callback)
-    },
-    "should respond with the correct dependencies": function (err, pkgs) {
-      assert.isNull(err);
-      assert.deepEqual(pkgs, rawPackages);
-    }
-  },
-  "the file() method": {
-    "when passed a valid file": {
+    "the dir() method": {
       topic: function () {
-        analyzer.file({ target: path.join(__dirname, '..', 'lib', 'require-analyzer') }, this.callback)
+        var that = this;
+        analyzer.dir({ target: path.join(__dirname, '..', 'lib') }, this.callback);
       },
       "should respond with the correct dependencies": function (err, pkgs) {
         assert.isNull(err);
         assert.deepEqual(pkgs, rawPackages);
       }
     },
-    "when passed a file with errors": {
+    "the package() method": {
       topic: function () {
-        analyzer.file({ target: path.join(__dirname, 'fixtures', 'throw-error') }, this.callback)
+        analyzer.package({ target: path.join(__dirname, '..') }, this.callback)
       },
-      "should respond with an error": function (err, pkgs) {
-        assert.isNotNull(err);
+      "should respond with the correct dependencies": function (err, pkgs) {
+        assert.isNull(err);
+        assert.deepEqual(pkgs, rawPackages);
+      }
+    },
+    "the file() method": {
+      "when passed a valid file": {
+        topic: function () {
+          analyzer.file({ target: path.join(__dirname, '..', 'lib', 'require-analyzer') }, this.callback)
+        },
+        "should respond with the correct dependencies": function (err, pkgs) {
+          assert.isNull(err);
+          assert.deepEqual(pkgs, rawPackages);
+        }
+      },
+      "when passed a file with errors": {
+        topic: function () {
+          analyzer.file({ target: path.join(__dirname, 'fixtures', 'throw-error') }, this.callback)
+        },
+        "should respond with an error": function (err, pkgs) {
+          assert.isNotNull(err);
+        }
       }
     }
   }
-}
 }).export(module);
